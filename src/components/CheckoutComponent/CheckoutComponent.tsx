800adcdf69eec7b972e60fc8219768885ade3033
Erik Målsäter <erik.malsater92@gmail.com>
2019-10-18 15:09:44 +0200

    adds more basic containers and styling

--- src/components/CheckoutComponent/CheckoutComponent.tsx
+++ src/components/CheckoutComponent/CheckoutComponent.tsx
@@ -7,7 +7,7 @@ class CheckoutComponent extends React.Component<{}, {}> {
       <div className="checkoutContainer">
         <div className="centeredContainer">
           <header>
-            <a href=""><img src={require("../../images/back-arrow-icon.png")} />Tillbehör</a>
+            <a href=""><img className="backArrowIcon" src={require("../../images/back-arrow-icon.png")} />Tillbehör</a>
             <h1>Kassa</h1>
           </header>
           <main>
@@ -22,14 +22,23 @@ class CheckoutComponent extends React.Component<{}, {}> {
            </div>
 
            <div className="cartContainer formContainer">
-
+              Produkter
            </div>
 
+           
+
           </main>
+          <footer>
+             <a href=""><img className="returnArrowIcon" src={require("../../images/return-arrow-icon.png")} alt=""/>Fortsätt handla</a>
+           </footer>
         </div>
+        
         <aside>
           <div className="keypadContainer">
             keypad
+          </div>
+          <div className="orderContainer">
+            <a href="">BESTÄLL</a>
           </div>
         </aside>
       </div>
